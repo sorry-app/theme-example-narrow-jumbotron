051b6b58a79ba415e3342894a4dcb3c41a0aa24f
Robert Rawlins <robert@weboffins.com>
2014-03-19 11:57:10 +0000

    Installed the sorry theme deployment grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,19 +23,46 @@ module.exports = function(grunt) {
         src: 'Gruntfile.js'
       }
     },
+
+    // Directory watching.
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
         tasks: ['jshint:gruntfile']
+      },
+      theme: {
+        files: 'src/**/*',
+        tasks: ['sorry_theme'],
+        options: {
+          interrupt: true,
+        }
       }
-    }
+    },
+
+    // Load in your sorry credentials.
+    // NOTE: NEVER CHECK YOUR CREDENTIALS INTO YOUR REPOSITORY.
+    sorry: grunt.file.readJSON('sorry.json'),
+
+    // Sorry theme deployment.
+    sorry_theme: {
+      default_options: {
+        options: {
+          username: '<%= sorry.username %>',
+          password: '<%= sorry.password %>',
+          page_id: 'my-first-status-page',
+          host: 'api.lvh.me',
+          port: 3000
+        }
+      }
+    },
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sorry-theme');
 
   // Default task.
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'sorry_theme']);
 
 };
